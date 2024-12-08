a32d682bc2f395932199d20ac5200937ae9db0b2
Niroshan <knniroshan82@outlook.com>
2024-12-08 18:02:05 +0530

    Database part added (UsedInMemoryDatabase)

---
+++ Data/ContactlyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudNet8.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet8.Data
+{
+    public class ContactlyDbContext : DbContext
+    {
+        public ContactlyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
